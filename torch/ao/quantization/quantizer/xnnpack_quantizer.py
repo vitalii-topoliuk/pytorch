f6c4177236568d8e735b2f7f1db0f7f2148a3bfb
Kimish Patel <kimishpatel@fb.com>
2023-08-18 16:19:42 -0700

    [Quantization] Make annotation util functions return annotated nodes
    Summary:
    Having annotation functions return nodes that are annotated is useful
    specifically for adding "quantization_tag" to those nodes
    
    Test Plan:
    CI
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 70c0736c547a9c73bc857ac94b3b2e177aa05438
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/107106

--- torch/ao/quantization/quantizer/xnnpack_quantizer.py
+++ torch/ao/quantization/quantizer/xnnpack_quantizer.py
@@ -381,7 +381,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         """
         Note: This is only used for QAT. In PTQ, batchnorm should already be fused into the conv.
         """
@@ -392,7 +392,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         """
         Note: This is only used for QAT. In PTQ, batchnorm should already be fused into the conv.
         """
@@ -403,7 +403,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         return OP_TO_ANNOTATOR["conv2d_relu"](gm, quantization_config, filter_fn)
 
     def _annotate_conv2d(
@@ -411,7 +411,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         return OP_TO_ANNOTATOR["conv2d"](gm, quantization_config, filter_fn)
 
     def _annotate_linear(
@@ -419,7 +419,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         return OP_TO_ANNOTATOR["linear"](gm, quantization_config, filter_fn)
 
     def _annotate_adaptive_avg_pool2d(
@@ -427,7 +427,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         return OP_TO_ANNOTATOR["adaptive_avg_pool2d"](
             gm, quantization_config, filter_fn
         )
@@ -438,7 +438,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         return OP_TO_ANNOTATOR["gru_io_only"](gm, quantization_config, filter_fn)
 
     def _annotate_max_pool2d(
@@ -446,7 +446,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         return OP_TO_ANNOTATOR["max_pool2d"](gm, quantization_config, filter_fn)
 
     def _annotate_add_patterns(
@@ -463,7 +463,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         return OP_TO_ANNOTATOR["add_relu"](gm, quantization_config, filter_fn)
 
     def _annotate_add(
@@ -471,7 +471,7 @@ class XNNPACKQuantizer(Quantizer):
         gm: torch.fx.GraphModule,
         quantization_config: Optional[QuantizationConfig],
         filter_fn: Optional[Callable[[Node], bool]] = None,
-    ) -> None:
+    ) -> List[List[Node]]:
         return OP_TO_ANNOTATOR["add"](gm, quantization_config, filter_fn)
 
     def validate(self, model: torch.fx.GraphModule) -> None:
